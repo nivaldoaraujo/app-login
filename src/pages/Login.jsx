cb6d57ed96dae19b75bb0f7382a8ad2d5511b5fc
Nivaldo Batista De Araujo <nbaraujo@firjan.com.br>
2026-03-05 15:21:24 -0300

    tela login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,57 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
 
 
 function Login() {
-  
+  const[login, setLogin] = useState("")
+  const[senha, setSenha] = useState("")
+  const [erro, setErro] = useState("")
+  const navigate = useNavigate()
+
+
+  async function validarLogin(e){
+
+    e.preventDefault()
+    const response = await fetch("http://localhost:3020/usuarios")
+
+    const usuarios = await response.json()
+
+    const usuarioValido = usuarios.find(
+      (u) => u.login === login && u.senha === senha
+    )
+
+    if(usuarioValido){
+      navigate("/home")
+    }else
+      {
+        setErro("Login ou senha errados!!")
+      }
+    }
+
 
   return (
     <>
     <h1>login</h1>
-    <Link to={"/cadastro"} >Cadastro</Link>
+    <form onSubmit={validarLogin} >
+       <input
+          type="text" 
+          placeholder="Login"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        /> <br /><br />
+        <input
+          type="password" 
+          placeholder="Senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        /> <br /><br />
+        <button type="submit" >Entrar</button>
+            
+    </form>
+    {erro && <p style={{color: "red"}}>{erro}</p>}
+    <Link to={"/cadastro"} >Cadastro</Link><br />
+ 
     </>
   )
 }
