f5405a1437261646066336867999888b070aba43
Nivaldo Batista De Araujo <nbaraujo@firjan.com.br>
2026-03-05 15:53:19 -0300

    tela cadastro

--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -1,13 +1,50 @@
-
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Cadastro() {
-  
+const[login, setLogin] = useState("")
+const[senha, setSenha] = useState("")
+const navigate = useNavigate()
 
-  return (
-    <>
-    <h1>Cadastro</h1>
-    </>
-  )
+async function cadastrar(e){
+  e.preventDefault()
+  await fetch("http://localhost:3020/usuarios",{
+    method: "POST",
+    headers:{
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      login,
+      senha
+    })
+  })
+  alert("Usuário cadastrado!!")
+  navigate("/")
 }
 
-export default Cadastro
+  return (
+    <div>
+      <h2>Novo Cadastro</h2>
+
+      <form onSubmit={cadastrar}>
+        <input
+          type="text"
+          placeholder="Login"
+          value={login}
+          onChange={(e) => setLogin(e.target.value)}
+        /> <br /><br />
+
+        <input
+          type="password"
+          placeholder="Senha"
+          value={senha}
+          onChange={(e) => setSenha(e.target.value)}
+        /> <br /><br />
+
+        <button type="submit">Cadastrar</button>
+      </form>
+    </div>
+  );
+}
+
+export default Cadastro;
